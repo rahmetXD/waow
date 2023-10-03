c5392b3676b4f26e0d0f8b2aff0403564601f15e
rahmetXD <144803190+rahmetXD@users.noreply.github.com>
2023-10-04 00:55:18 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -6,10 +6,10 @@ class Config():
     # https://my.telegram.org
     API_ID = int(os.environ.get("API_ID","18049084"))
     API_HASH = os.environ.get("API_HASH","7e74b1e22026fcc291d32b3d431aa21e")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN","6645400469:AAGPSg7oAvHyBm2xUGM8n-Co7IxmDYSJJX4")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME","AhriV2_bot")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN","6445812901:AAGWxaM8hfQtDAphUnrRHsFRT12KOwhuwck")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME","AhriTaggerBot")
     BOT_NAME = os.environ.get("BOT_NAME","Ahri")
-    BOT_ID = int(os.environ.get("BOT_ID","6645400469"))
+    BOT_ID = int(os.environ.get("BOT_ID","6445812901"))
     SUDO_USERS = os.environ.get("SUDO_USERS","rahmetinc").split()
     SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT","redhackarsiv")
     OWNER_ID = int(os.environ.get("OWNER_ID","5944841427"))
