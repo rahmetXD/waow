e3b5600a62706029e61a3d850645e5ecbab9a8d9
rahmetXD <144803190+rahmetXD@users.noreply.github.com>
2023-10-09 17:19:28 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -6,7 +6,7 @@ class Config():
     # https://my.telegram.org
     API_ID = int(os.environ.get("API_ID","18049084"))
     API_HASH = os.environ.get("API_HASH","7e74b1e22026fcc291d32b3d431aa21e")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN","6445812901:AAGWxaM8hfQtDAphUnrRHsFRT12KOwhuwck")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN","6110153217:AAGY1JLW78Ng9gpIyBtLpK2NHB7U4NgNhZQ")
     BOT_USERNAME = os.environ.get("BOT_USERNAME","AhriTaggerBot")
     BOT_NAME = os.environ.get("BOT_NAME","Ahri")
     BOT_ID = int(os.environ.get("BOT_ID","6445812901"))
