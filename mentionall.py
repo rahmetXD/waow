bdebaee9116b1434f253050bd105466947fe80e6
rahmetXD <144803190+rahmetXD@users.noreply.github.com>
2023-10-10 18:02:53 +0300

    Update mentionall.py

--- mentionall.py
+++ mentionall.py
@@ -13,6 +13,8 @@ import pyrogram
 from youtubesearchpython import VideosSearch
 import youtube_dl
 import platform
+from telethon.tl import types
+from telethon.tl import functions, types
 
 logging.basicConfig(
     level=logging.INFO,
@@ -617,8 +619,6 @@ async def handler(event):
     else:
         await event.respond('Bu komutu kullanma izniniz yok!')
 
-from telethon.tl import functions, types
-
 @client.on(events.NewMessage(pattern='/grup'))
 async def grup_info(event):
     # Sadece grup ve kanallarda çalıştır
@@ -644,21 +644,17 @@ async def grup_info(event):
     group_name = chat.title
     group_id = chat.id
 
-    deleted_count = await client(functions.channels.GetParticipantCountRequest(
-        channel=group_id,
-        filter=types.ChannelParticipantsSearchEmpty()
-    ))
+    # Grup bilgilerini al
+    chat_info = await event.client.get_entity(group_id)
 
-    active_count = await client(functions.channels.GetParticipantCountRequest(
-        channel=group_id,
-        filter=types.ChannelParticipantsSearch()
-    ))
+    # Diğer bilgileri al
+    deleted_count = chat_info.participants_count - chat_info.participants_count_with_deleted
+    active_count = chat_info.participants_count - chat_info.kicked_count - chat_info.left_count
+    bot_count = sum(1 for participant in chat_info.members if participant.bot)
+    total_count = chat_info.participants_count
 
-    participants = await client.get_participants(group_id)
-    bot_count = sum(1 for participant in participants if participant.bot)
-    total_count = len(participants)
-
-    chat_admins = await client.get_participants(group_id, filter=types.ChannelParticipantsAdmins)
+    # Kurucuyu bul
+    chat_admins = await event.client.get_participants(group_id, filter=types.ChannelParticipantsAdmins)
     for admin in chat_admins:
         if admin.creator:
             founder = admin
@@ -666,8 +662,8 @@ async def grup_info(event):
     response_text = (
         f'➻ Grup Adı: {group_name}\n'
         f'➻ Grup ID: {group_id}\n'
-        f'➻ Delete Hesap: {deleted_count.count}\n'
-        f'➻ Aktif Kullanıcıları: {active_count.count}\n'
+        f'➻ Delete Hesap: {deleted_count}\n'
+        f'➻ Aktif Kullanıcıları: {active_count}\n'
         f'➻ Bot Sayısı: {bot_count}\n'
         f'➻ Grup Üye Sayısı: {total_count}\n'
         f'➻ Kurucu: @{founder.username}'
@@ -675,7 +671,6 @@ async def grup_info(event):
 
     # Bilgileri yanıt olarak gönder
     await event.respond(response_text)
-
 
 print("Ahri Tagger AKtif, Sağol Sahip! @rahmetiNC ✨")
 client.run_until_disconnected()
